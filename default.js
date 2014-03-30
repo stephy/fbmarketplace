82f91a2bf95d825e8ee676727ac81e9f8dc22d1f
Stephani Alves <gosling@Stephanis-MacBook-Pro.local>
2014-03-29 17:23:12 -0700

    fixing hashtag

--- default.js
+++ default.js
@@ -1,101 +1,9 @@
 //overwrite array prototype
 // attach the .compare method to Array's prototype to call it on any array
-Array.prototype.compare = function (array) {
-    // if the other array is a falsy value, return
-    if (!array)
-        return false;
-
-    // compare lengths - can save a lot of time
-    if (this.length != array.length)
-        return false;
-
-    for (var i = 0, l=this.length; i < l; i++) {
-        // Check if we have nested arrays
-        if (this[i] instanceof Array && array[i] instanceof Array) {
-            // recurse into the nested arrays
-            if (!this[i].compare(array[i]))
-                return false;
-        }
-        else if (this[i] != array[i]) {
-            // Warning - two different object instances will never be equal: {x:20} != {x:20}
-            return false;
-        }
-    }
-    return true;
-}
-
 
 $(function(){
 
-function FbMarketplace(){
-	/*
-	 * insert market place wrapper
-	 */
-	var rightbar = $('#pagelet_reminders');
-	var mpwrapper = $('<div>', {
-	  id: 'FbMarketplace',
-	  css:{
-	    'cursor': 'pointer'
-	  }
-	});
-
-	var title = $('<div>', {
-	  class: 'uiHeader uiHeaderTopBorder mbm pbs uiSideHeader',
-	  html: '<h6 class="uiHeaderTitle" id="fbmpnav-title">Marketplace</h6>',
-	  css:{
-	    'cursor': 'pointer'
-	  }
-	});
-	mpwrapper.append(title);
-	mpwrapper.insertAfter(rightbar);
-
-	/*
-	 * insert market button
-	 */
-	//get status input box
-	var inputBox = $('.mentionsTextarea');
-	console.log('Input box:', inputBox);
-
-	//add event to input box
-	inputBox.on('click', function(){
-		
-		//watch for hashtag 
-		var textarea = $('.mentionsTextarea');
-		//#fbmarketplace
-		var hashtagQueue = [35,102,98,109,97,114,107,101,116,112,108,97,99,101];
-		var hashPositions = [];
-		var currentHash = [];
-
-		textarea.keypress(function(e){
-			currentHash.push(e.which);
-			if(e.which === 35){ //start hashtag (entered hashtag)
-				var index = textarea.val().length;
-				console.log('index: ', index);
-				hashPositions.push(index);
-				console.log(hashPositions);
-			}else if(e.which === 32){//end of hashtag (entered spacebar)
-				console.log('spacebar');
-				console.log('currentHash: ', currentHash);
-				//check hashtags?
-				if(hashPositions.length>0){ //there's a hashtag
-					var tmp = hashtagQueue.slice(0);
-					//can we possible have #fbmarketplace?
-					var remaingOffset = textarea.val().length - hashPositions[0];
-					console.log('remaingOffset', remaingOffset);
-					if(remaingOffset === hashtagQueue.length){
-						var equal = currentHash.compare(hashtagQueue);
-						console.log('are they the same?', equal)
-					}//end if possible tag
-					else{
-						hashPositions.pop();
-					}
-					
-				}//hashPositions.length>0
-
-				
-			};
-
-		})
+function addtagbutton(){
 		//if hashtag is typed button will be added
 		//get FB actionBar
 		var actionBar = $("._1dsp._4-");
@@ -128,7 +36,6 @@ function FbMarketplace(){
 				//add event handler for post button
 				var buttonPost = $('._42ft._4jy0._11b._4jy3._4jy1.selected');
 				buttonPost.on('click', function(){
-					console.log("submit");
 					var fbmpItemwrapper = $('<div>', {
 					  class: 'item-wrapper',
 					  css:{
@@ -143,15 +50,88 @@ function FbMarketplace(){
 
 					var t = $('#fbmpnav-title');
 					var title = $('#FbMarketplace-title').val();
-					console.log('title:', title);
+		
 					fbmpItemwrapper.append(fbmpItemTitle);
 					fbmpItemwrapper.insertAfter(t);
-					console.log(fbmpItemwrapper);
+				
 					
 				});
 			});
 			mpbutton.prependTo(actionBar);
 		}
+}
+
+function FbMarketplace(){
+	/*
+	 * insert market place wrapper
+	 */
+	var rightbar = $('#pagelet_reminders');
+	var mpwrapper = $('<div>', {
+	  id: 'FbMarketplace',
+	  css:{
+	    'cursor': 'pointer'
+	  }
+	});
+
+	var title = $('<div>', {
+	  class: 'uiHeader uiHeaderTopBorder mbm pbs uiSideHeader',
+	  html: '<h6 class="uiHeaderTitle" id="fbmpnav-title">Marketplace</h6>',
+	  css:{
+	    'cursor': 'pointer'
+	  }
+	});
+	mpwrapper.append(title);
+	mpwrapper.insertAfter(rightbar);
+
+	/*
+	 * insert market button
+	 */
+	//get status input box
+	var inputBox = $('.mentionsTextarea');
+	//#fbmarketplace
+	var hashtagQueue = [102,98,109,97,114,107,101,116,112,108,97,99,101];
+	var hashPositions = [];
+	var currentHash = [];
+	//add event to input box
+	inputBox.on('click', function(){
+		
+		//watch for hashtag 
+		var textarea = $('.mentionsTextarea');
+		
+
+		textarea.keypress(function(e){
+			
+			if(e.which === 35){ //start hashtag (entered hashtag)
+				currentHash.push(e.which);
+				var index = textarea.val().length;
+				//console.log('index: ', index);
+				hashPositions.push(index);
+				//console.log(hashPositions);
+			}else if(e.which === 32){//end of hashtag (entered spacebar)
+				addtagbutton();
+				//console.log('spacebar');
+				//console.log('currentHash: ', currentHash);
+				//check hashtags?
+				if(hashPositions.length>0){ //there's a hashtag
+					var tmp = hashtagQueue.slice(0);
+					//can we possible have #fbmarketplace?
+					var remaingOffset = (textarea.val().length-1) - hashPositions[0];
+					//console.log('remaingOffset', remaingOffset);
+					if(remaingOffset === hashtagQueue.length){
+						//var equal = currentHash.compare(hashtagQueue);
+						//console.log('are they the same?', equal)
+					}//end if possible tag
+					else{
+						hashPositions.pop();
+					}
+					
+				}//hashPositions.length>0
+
+				
+			};
+
+		});
+
 		
 
 
